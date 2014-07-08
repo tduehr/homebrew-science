3ab6f20c08a4042ecb59ec6ad2b70eb296c77190
tduehr <tduehr@gmail.com>
2014-07-08 08:45:41 -0500

    new formula: sigrok-firmware-fx2lafw 0.1.1

---
+++ sigrok-firmware-fx2lafw.rb
@@ -0,0 +1,30 @@
+require 'formula'
+
+class SigrokDownloadStrategy < GitDownloadStrategy
+  def support_depth?
+    false
+  end
+end
+
+class SigrokFirmwareFx2lafw < Formula
+  url 'http://sigrok.org/download/source/sigrok-firmware-fx2lafw/sigrok-firmware-fx2lafw-0.1.1.tar.gz'
+  homepage 'http://sigrok.org/'
+  head 'git://sigrok.org/sigrok-firmware-fx2lafw',:using => SigrokDownloadStrategy
+  sha1 'aac6f8f827e7446a5dabc6f82a8620372fd340e6'
+
+  depends_on 'glib'
+  depends_on 'libtool' => :build
+  depends_on 'sdcc'
+  depends_on 'pkg-config' => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  def install
+    if build.head?
+      system "./autogen.sh"
+    end
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+end
