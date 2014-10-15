f882ffc2c481ea63a0fada87e3e3aba3229ef2b6
tduehr <tduehr@gmail.com>
2014-10-15 10:21:31 -0500

    libsigrokdecode

--- libsigrokdecode.rb
+++ libsigrokdecode.rb
@@ -9,16 +9,14 @@ end
 class Libsigrokdecode < Formula
   url 'http://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.3.0.tar.gz'
   homepage 'http://sigrok.org/'
-  head 'git://sigrok.org/libsigrokdecode', :using => SigrokDownloadStrategy
+  head 'git://sigrok.org/libsigrokdecode',:using => SigrokDownloadStrategy
   sha1 'a75f2839cf62d965281bac22919e761c5210e32e'
 
+  depends_on 'libsigrok'
   depends_on 'glib'
-  depends_on 'libzip'
-  depends_on 'python3'
-  depends_on 'libftdi0' => :optional
-  depends_on 'libusb' => :recommended
   depends_on 'pkg-config' => :build
   depends_on 'check' => :optional
+  depends_on 'python3'
 
   if build.head?
     depends_on 'libtool' => :build
@@ -26,34 +24,38 @@ class Libsigrokdecode < Formula
     depends_on 'automake' => :build
   end
 
-  option 'with-libserialport'
-  option 'with-librevisa'
+  option 'with-libserialport', 'Build with libserialport'
+  option 'with-librevisa', 'Build with librevisa'
 
-  resource 'librevisa' do
-    if build.head?
+  head do
+    resource 'librevisa' do
       url 'http://www.librevisa.org/git/librevisa.git'
-    else
-      url 'http://www.librevisa.org/download/librevisa-0.0.20130412.tar.gz'
-    end
-    sha1 '1bb24a6721cc994494f294fd86c78594ba4f8b40'
-  end if build.with? 'librevisa'
+    end if build.with? 'librevisa'
 
-  resource 'libserialport' do
-    if build.head?
+    resource 'libserialport' do
       url 'git://sigrok.org/libserialport', :using => SigrokDownloadStrategy
-    else
-      url 'http://sigrok.org/download/source/libserialport/libserialport-0.1.0.tar.gz'
-    end
-    sha1 'f8677c9e63caf1f6e6cb6aa39a8ae3a256516d78'
-  end if build.with? 'libserialport'
+    end if build.with? 'libserialport'
 
-  resource 'libsigrok' do
-    if build.head?
+    resource 'libsigrok' do
       url 'git://sigrok.org/libsigrok', :using => SigrokDownloadStrategy
-    else
-      url 'http://sigrok.org/download/source/libsigrok/libsigrok-0.3.0.tar.gz'
     end
-    sha1 '140db550820f98042d95432a87a9d7e3078b1490'
+  end
+
+  stable do
+    resource 'librevisa' do
+      url 'http://www.librevisa.org/download/librevisa-0.0.20130412.tar.gz'
+      sha1 '1bb24a6721cc994494f294fd86c78594ba4f8b40'
+    end if build.with? 'librevisa'
+
+    resource 'libserialport' do
+      url 'http://sigrok.org/download/source/libserialport/libserialport-0.1.0.tar.gz'
+      sha1 'f8677c9e63caf1f6e6cb6aa39a8ae3a256516d78'
+    end if build.with? 'libserialport'
+
+    resource 'libsigrok' do
+      url 'http://sigrok.org/download/source/libsigrok/libsigrok-0.3.0.tar.gz'
+      sha1 '140db550820f98042d95432a87a9d7e3078b1490'
+    end
   end
 
   def install
@@ -63,18 +65,21 @@ class Libsigrokdecode < Formula
     ]
 
     ENV.append_path('PKG_CONFIG_PATH', lib/'pkgconfig')
+    ENV.append_path "PKG_CONFIG_PATH", HOMEBREW_PREFIX/"Frameworks/Python.framework/Versions/3.4/lib/pkgconfig"
 
     resource('libserialport').stage do
       system './autogen.sh' if build.head?
       system './configure', *common_args
       system 'make', 'install'
+      ENV.append_path('PKG_CONFIG_PATH', lib/'pkgconfig')
     end if build.with? 'libserialport'
 
     resource('librevisa').stage do
       system "./autogen.sh" if build.head?
       system "./configure", *common_args
       system "make", "install"
-    end
+      ENV.append_path('PKG_CONFIG_PATH', lib/'pkgconfig')
+    end if build.with? 'librevisa'
 
     # not strictly needed for libsigrokdecode but both sigrok-cli and pulseview
     # need it so we install it here as a resource to avoid collisions
@@ -82,21 +87,22 @@ class Libsigrokdecode < Formula
       system "./autogen.sh" if build.head?
       system "./configure", *common_args
       system "make", "install"
-      system "ln", "-s", "/usr/local/share/", share
+      share.install
+      ENV.append_path('PKG_CONFIG_PATH', lib/'pkgconfig')
     end
 
-    qt = Formula['qt'].opt_prefix
-    args = std_cmake_args + %W[
-      -DPNG_INCLUDE_DIR=#{MacOS::X11.include}
-      -DALTERNATIVE_QT_INCLUDE_DIR=#{qt}/include
-      -DQT_SRC_DIR=#{qt}/src
-    ]
+    # qt = Formula['qt'].opt_prefix
+    # args = std_cmake_args + %W[
+    #   -DPNG_INCLUDE_DIR=#{MacOS::X11.include}
+    #   -DALTERNATIVE_QT_INCLUDE_DIR=#{qt}/include
+    #   -DQT_SRC_DIR=#{qt}/src
+    # ]
+    #
+    # python_prefix = `python3-config --prefix`.strip
+    # args << '-DPYTHON_LIBRARY=#{python_prefix}/Python'
+    # args << '-DPYTHON_INCLUDE_DIR=#{python_prefix}/Headers'
 
-    python_prefix = `python3-config --prefix`.strip
-    args << '-DPYTHON_LIBRARY=#{python_prefix}/Python'
-    args << '-DPYTHON_INCLUDE_DIR=#{python_prefix}/Headers'
-    ENV['python3_CFLAGS'] = `python3-config --cflags`
-    ENV['python3_LIBS'] = `python3-config --libs`.gsub('-lpython3.4', '')
+    ENV.deparallelize
 
     system './autogen.sh' if build.head?
     system './configure', *common_args
